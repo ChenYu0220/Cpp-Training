fdb5f700baf5c54c6a61a424ef51c1af501f875a
chenyu <cy1273971423@gmail.com>
2026-01-21 00:56:19 +0800

    feat: 附计算环入口的思路

--- CheckRingInLinkList/CheckRingInLinkList/LinkList.h
+++ CheckRingInLinkList/CheckRingInLinkList/LinkList.h
@@ -120,3 +120,14 @@ public:
 		}
 	}
 };
+
+/*
+* 第一次相遇慢指针走了 L+X ,快指针走了 2(L+X);快指针比慢指针多走n圈所以 快指针走了 L+X+nC
+* 所以 L+X+nC = 2(L+X)
+* 所以 L+X = nC
+* L = nC - X
+* L = (n-1)C + (C - X)
+* 这意味着从头节点出发到环起点的距离 L 等于从相遇点出发沿环走 (n-1) 圈再加上从相遇点到环起点的距离 (C - X)
+* 当 n-1 = 0 时
+* 【寻找链表中环的起点 | 链表面试题】https://www.bilibili.com/video/BV1gJbnzwEqr?vd_source=c08e421cbc7550697c2e21c0ded8ee0b
+*/
